6b863d5f676d18d817cf2d0e5d73623e1fb2e669
May Ng <mngmay@gmail.com>
2019-06-19 15:50:00 -0700

    Adding components of stretch - not finalized yet.  Adding close buttons to remove articles.  Playing around a bit with animations

--- Article/Article.js
+++ Article/Article.js
@@ -12,11 +12,19 @@ class Article {
 
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     this.expandButton.addEventListener('click', () => this.expandArticle());
+
+    this.closeBtn = this.domElement.querySelector('.close');
+    console.log(this.closeBtn);
+    this.closeBtn.addEventListener('click', () => this.closeArticle());
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
     this.domElement.classList.toggle('article-open');
+  }
+
+  closeArticle() {
+    this.domElement.style.visibility = "hidden";
   }
 }
 
@@ -33,3 +41,15 @@ let articles = document.querySelectorAll('.article');
 articles.forEach(function(article){
   return new Article(article);
 });
+
+
+//STRETCH!!! 
+
+//Add button to articles
+articles.forEach(article => {
+  const closeBtn = document.createElement("button");
+  closeBtn.textContent = "Close";
+  closeBtn.className = "article close";
+  article.appendChild(closeBtn)
+});
+
--- Menu/Menu.js
+++ Menu/Menu.js
@@ -2,7 +2,7 @@
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence. 
   menu.classList.toggle('menu--open');
-  console.log(menu);
+  // console.log(menu); just testin
 }
 
 // Start Here: Create a reference to the ".menu" class
@@ -13,3 +13,6 @@ const menuButton = document.querySelector('.menu-button');
 
 // Using your menuButton reference, add a click handler that calls toggleMenu
 menuButton.addEventListener('click', () => toggleMenu());
+
+
+TweenMax.to(".menu", 10, {x: 0, ease:Elastic.easeOut});
