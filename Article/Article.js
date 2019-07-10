b9366725708c7f5a5142cea246c48d1d0b6a17bc
May Ng <mngmay@gmail.com>
2019-07-10 14:08:14 -0700

    Only relevant article is on display when clicked. other articles close when a new one is selected.

--- Article/Article.js
+++ Article/Article.js
@@ -2,37 +2,88 @@
 
 const articleData = [
   {
-    title: "Lambda School Students: We're the best!",
-    content: "Test",
-    date: "Nov 5th, 2017"
-  },
-
-  {
-    title: "Javascript and You, ES6",
+    title: `Lambda School Students: "We're the best!"`,
+    date: "Nov 5th, 2017",
     content: [
-      "Alohamora wand elf parchment, Wingardium Leviosa hippogriff, house\
-dementors betrayal. Holly, Snape centaur portkey ghost Hermione spell\
-  bezoar Scabbers. Peruvian-Night-Powder werewolf, Dobby pear-tickle\
-  half-moon-glasses, Knight-Bus. Padfoot snargaluff seeker: Hagrid\
-  broomstick mischief managed. Snitch Fluffy rock-cake, 9 ¾ dress robes\
-  I must not tell lies. Mudbloods yew pumpkin juice phials Ravenclaw’s\
-  Diadem 10 galleons Thieves Downfall. Ministry-of-Magic mimubulus\
-  mimbletonia Pigwidgeon knut phoenix feather other minister Azkaban.\
-  Hedwig Daily Prophet treacle tart full-moon Ollivanders You-Know-Who\
-  cursed. Fawkes maze raw-steak Voldemort Goblin Wars snitch Forbidden\
-  forest grindylows wool socks.",
-      "Boggarts lavender robes, Hermione Granger Fantastic Beasts and Where\
-  to Find Them. Bee in your bonnet Hand of Glory elder wand, spectacles\
-  House Cup Bertie Bott’s Every Flavor Beans Impedimenta. Stunning\
-  spells tap-dancing spider Slytherin’s Heir mewing kittens Remus Lupin.\
-  Palominos scarlet train black robes, Metamorphimagus Niffler dead easy\
-  second bedroom. Padma and Parvati Sorting Hat Minister of Magic blue\
-  turban remember my last."
-    ],
-    date: "Nov 7th, 2017"
+      `Lucas ipsum dolor sit amet ben twi'lek padmé darth darth darth moff hutt organa twi'lek. Ben amidala secura skywalker lando
+     moff wicket tatooine luke. Solo wampa wampa calrissian yoda moff. Darth grievous darth gonk darth hutt. Darth baba skywalker
+     watto fett jango maul han. Mon ewok sidious sidious lando kenobi grievous gamorrean solo. Yoda wedge utapau darth calamari.
+     Hutt calamari darth jabba. Darth dooku amidala organa moff. Boba darth binks solo hutt skywalker dantooine skywalker. Qui-gonn
+     jar twi'lek jinn leia jango skywalker mon.`,
+      `Grievous fett calamari anakin skywalker hutt. Alderaan darth kenobi darth r2-d2
+     windu mothma. Sidious darth calamari moff. Wampa mothma sith wedge solo mara. Darth gonk maul sith moff chewbacca palpatine
+     mace amidala. C-3po solo skywalker anakin yoda leia. Maul wampa bespin watto jade ewok darth jabba. Lando dantooine moff
+     k-3po dantooine luke. Fisto mandalore darth wedge c-3p0 ahsoka. Secura moff palpatine fett. Anakin sith darth darth. Moff
+     solo leia ben ponda jade. Binks jango aayla skywalker skywalker cade. Mustafar darth ventress anakin watto. Yavin jawa sebulba
+     owen jinn tatooine sith organa.`,
+      `Dagobah hutt jawa leia calamari ventress skywalker yoda. Binks wicket hutt coruscant sidious
+     naboo ackbar tatooine. Hutt lars padmé darth. Maul solo darth darth jabba qui-gon chewbacca darth maul. Moff baba wicket
+     han. C-3po antilles moff qui-gon ahsoka aayla dooku amidala. Palpatine droid amidala droid k-3po twi'lek padmé wookiee. Leia
+     moff calamari mon obi-wan. Solo grievous lando coruscant. Jinn darth palpatine obi-wan mon.`
+    ]
+  },
+  {
+    title: `Javascript and You, ES6`,
+    date: `Nov 7th, 2017`,
+    content: [
+      `Alohamora wand elf parchment, Wingardium Leviosa hippogriff, house dementors betrayal. Holly, Snape centaur portkey ghost
+     Hermione spell bezoar Scabbers. Peruvian-Night-Powder werewolf, Dobby pear-tickle half-moon-glasses, Knight-Bus. Padfoot
+     snargaluff seeker: Hagrid broomstick mischief managed. Snitch Fluffy rock-cake, 9 ¾ dress robes I must not tell lies. Mudbloods
+     yew pumpkin juice phials Ravenclaw's Diadem 10 galleons Thieves Downfall. Ministry-of-Magic mimubulus mimbletonia Pigwidgeon
+     knut phoenix feather other minister Azkaban. Hedwig Daily Prophet treacle tart full-moon Ollivanders You-Know-Who cursed.
+     Fawkes maze raw-steak Voldemort Goblin Wars snitch Forbidden forest grindylows wool socks.`,
+      `Boggarts lavender robes, Hermione Granger Fantastic Beasts and Where to Find Them. Bee in your bonnet Hand of Glory elder
+     wand, spectacles House Cup Bertie Bott's Every Flavor Beans Impedimenta. Stunning spells tap-dancing spider Slytherin's Heir
+     mewing kittens Remus Lupin. Palominos scarlet train black robes, Metamorphimagus Niffler dead easy second bedroom. Padma
+     and Parvati Sorting Hat Minister of Magic blue turban remember my last.`
+    ]
+  },
+  {
+    title: `React vs Angular vs Vue`,
+    date: "Nov 7th, 2017",
+    content: [
+      `Bulbasaur Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ivysaur Lorem ipsum dolor sit amet, consectetur adipiscing
+     elit. Venusaur Lorem ipsum dolor sit amet, consectetur adipiscing elit. Charmander Lorem ipsum dolor sit amet, consectetur
+     adipiscing elit. Charmeleon Lorem ipsum dolor sit amet, consectetur adipiscing elit. Charizard Lorem ipsum dolor sit amet,
+     consectetur adipiscing elit. Squirtle Lorem ipsum dolor sit amet, consectetur adipiscing elit. Wartortle Lorem ipsum dolor
+     sit amet, consectetur adipiscing elit. Blastoise Lorem ipsum dolor sit amet, consectetur adipiscing elit. Caterpie Lorem
+     ipsum dolor sit amet, consectetur adipiscing elit. Metapod Lorem ipsum dolor sit amet, consectetur adipiscing elit. Butterfree
+     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Weedle Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+     Kakuna Lorem ipsum dolor sit amet, consectetur adipiscing elit. Beedrill Lorem ipsum dolor sit amet, consectetur adipiscing
+     elit`,
+      `Pidgey Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pidgeotto Lorem ipsum dolor sit amet, consectetur adipiscing
+     elit. Pidgeot Lorem ipsum dolor sit amet, consectetur adipiscing elit. Rattata Lorem ipsum dolor sit amet, consectetur adipiscing
+     elit. Raticate Lorem ipsum dolor sit amet, consectetur adipiscing elit. Spearow Lorem ipsum dolor sit amet, consectetur adipiscing
+     elit. Fearow Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ekans Lorem ipsum dolor sit amet, consectetur adipiscing
+     elit. Arbok Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pikachu Lorem ipsum dolor sit amet, consectetur adipiscing
+     elit. Raichu Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sandshrew Lorem ipsum dolor sit amet, consectetur adipiscing
+     elit. Sandslash Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nidoran Lorem ipsum dolor sit amet, consectetur
+     adipiscing elit. Nidorina Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nidoqueen Lorem ipsum dolor sit amet,
+     consectetur adipiscing elit. Nidoran Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nidorino Lorem ipsum dolor
+     sit amet, consectetur adipiscing elit. Nidoking Lorem ipsum`
+    ]
+  },
+  {
+    title: `Professional Software Development in 2018`,
+    date: "Nov 7th, 2017",
+    content: [
+      `Hodor hodor HODOR! Hodor hodor - hodor, hodor. Hodor hodor... Hodor hodor hodor; hodor hodor. Hodor hodor hodor, hodor, hodor
+     hodor. Hodor, hodor. Hodor. Hodor, hodor - hodor... Hodor hodor hodor; hodor HODOR hodor, hodor hodor?! Hodor hodor, hodor.
+     Hodor hodor hodor hodor hodor! Hodor hodor - HODOR hodor, hodor hodor hodor hodor hodor; hodor hodor? `,
+      `Hodor, hodor. Hodor. Hodor, hodor, hodor. Hodor hodor, hodor. Hodor hodor, hodor, hodor hodor. Hodor! Hodor hodor, hodor;
+     hodor hodor hodor? Hodor, hodor. Hodor. Hodor, hodor - HODOR hodor, hodor hodor hodor! Hodor, hodor. Hodor. Hodor, HODOR
+     hodor, hodor hodor, hodor, hodor hodor. Hodor hodor - hodor - hodor... Hodor hodor hodor hodor hodor hodor hodor?! Hodor
+     hodor - hodor hodor hodor. Hodor. Hodor hodor... Hodor hodor hodor hodor hodor?`,
+      `Hodor hodor - hodor... Hodor hodor hodor hodor. Hodor. Hodor! Hodor hodor, hodor hodor hodor hodor hodor; hodor hodor? Hodor!
+     Hodor hodor, HODOR hodor, hodor hodor?! Hodor! Hodor hodor, HODOR hodor, hodor hodor, hodor, hodor hodor. Hodor, hodor.
+     Hodor. Hodor, hodor, hodor. Hodor hodor... Hodor hodor hodor?! Hodor, hodor... Hodor hodor HODOR hodor, hodor hodor. Hodor.`,
+      `Hodor, hodor. Hodor. Hodor, hodor, hodor. Hodor hodor, hodor. Hodor hodor, hodor, hodor hodor. Hodor! Hodor hodor, hodor;
+     hodor hodor hodor? Hodor, hodor. Hodor. Hodor, hodor - HODOR hodor, hodor hodor hodor! Hodor, hodor. Hodor. Hodor, HODOR
+     hodor, hodor hodor, hodor, hodor hodor. Hodor hodor - hodor - hodor... Hodor hodor hodor hodor hodor hodor hodor?! Hodor
+     hodor - hodor hodor hodor. Hodor. Hodor hodor... Hodor hodor hodor hodor hodor?`
+    ]
   }
 ];
-
 const articles = document.querySelector(".articles");
 
 //test article creation
@@ -83,6 +134,11 @@ function createArticle(title, content, date) {
 
   //button events
   expandBtn.addEventListener("click", () => {
+    //close all articles so multiple cannot be open at the same time
+    let allArticles = document.querySelectorAll(".article");
+    allArticles.forEach(article => article.classList.remove("article-open"));
+
+    //open the relevant article
     articleContent.classList.toggle("hide");
     article.classList.toggle("article-open");
 
@@ -91,7 +147,6 @@ function createArticle(title, content, date) {
     } else {
       expandBtn.textContent = "Expand";
     }
-    console.log(articleContent);
   });
 
   removeBtn.addEventListener("click", () => {
