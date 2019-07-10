8df3905ba4b5ca401fe530761ebd90f800b2b541
May Ng <mngmay@gmail.com>
2019-07-10 13:47:21 -0700

    Expand button now changes textContent to Hide if article is expanded

--- Article/Article.js
+++ Article/Article.js
@@ -1,10 +1,49 @@
 //Refactor original assignment using functional components only
 
+const articleData = [
+  {
+    title: "Lambda School Students: We're the best!",
+    content: "Test",
+    date: "Nov 5th, 2017"
+  },
+
+  {
+    title: "Javascript and You, ES6",
+    content: [
+      "Alohamora wand elf parchment, Wingardium Leviosa hippogriff, house\
+dementors betrayal. Holly, Snape centaur portkey ghost Hermione spell\
+  bezoar Scabbers. Peruvian-Night-Powder werewolf, Dobby pear-tickle\
+  half-moon-glasses, Knight-Bus. Padfoot snargaluff seeker: Hagrid\
+  broomstick mischief managed. Snitch Fluffy rock-cake, 9 ¾ dress robes\
+  I must not tell lies. Mudbloods yew pumpkin juice phials Ravenclaw’s\
+  Diadem 10 galleons Thieves Downfall. Ministry-of-Magic mimubulus\
+  mimbletonia Pigwidgeon knut phoenix feather other minister Azkaban.\
+  Hedwig Daily Prophet treacle tart full-moon Ollivanders You-Know-Who\
+  cursed. Fawkes maze raw-steak Voldemort Goblin Wars snitch Forbidden\
+  forest grindylows wool socks.",
+      "Boggarts lavender robes, Hermione Granger Fantastic Beasts and Where\
+  to Find Them. Bee in your bonnet Hand of Glory elder wand, spectacles\
+  House Cup Bertie Bott’s Every Flavor Beans Impedimenta. Stunning\
+  spells tap-dancing spider Slytherin’s Heir mewing kittens Remus Lupin.\
+  Palominos scarlet train black robes, Metamorphimagus Niffler dead easy\
+  second bedroom. Padma and Parvati Sorting Hat Minister of Magic blue\
+  turban remember my last."
+    ],
+    date: "Nov 7th, 2017"
+  }
+];
+
 const articles = document.querySelector(".articles");
 
+//test article creation
 articles.appendChild(
   createArticle("Refactored Title", "Test Content", "July, 10th, 2019")
 );
+
+//grab data from JSON
+articleData.forEach(data => {
+  articles.appendChild(createArticle(data.title, data.content, data.date));
+});
 
 function createArticle(title, content, date) {
   //define new elements
@@ -46,6 +85,13 @@ function createArticle(title, content, date) {
   expandBtn.addEventListener("click", () => {
     articleContent.classList.toggle("hide");
     article.classList.toggle("article-open");
+
+    if (!articleContent.classList.contains("hide")) {
+      expandBtn.textContent = "Hide";
+    } else {
+      expandBtn.textContent = "Expand";
+    }
+    console.log(articleContent);
   });
 
   removeBtn.addEventListener("click", () => {
