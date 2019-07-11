9ebcf67a1de6cb112650b17b176749bb08100dcf
May Ng <mngmay@gmail.com>
2019-07-10 18:58:15 -0700

    Added add article to menu and modal pop up

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -14,3 +14,29 @@ const menuButton = document.querySelector(".menu-button");
 menuButton.addEventListener("click", () => toggleMenu());
 
 // TweenMax.to(".menu", 10, {x: 0, ease:Elastic.easeOut});
+
+//Modal for adding articles
+// Get the modal
+const modal = document.querySelector(".modal");
+// Get the button that opens the modal
+const addArticle = document.querySelector(".addArticle");
+// Get the <span> element that closes the modal
+const span = document.querySelector(".close");
+
+// When the user clicks on the button, open the modal
+addArticle.onclick = function() {
+  modal.style.display = "block";
+  menu.style.display = "none";
+  console.log(addArticle);
+  console.log(modal);
+};
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+};
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
